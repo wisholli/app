303711eb50d4772bbecf0c912dc584d5a76e5440
wisholli <osribna4@gmail.com>
2022-11-09 16:44:58 +0200

    Added componentDidMount

--- src/components/FindUserPage/FindUsersPageClassComponent.js
+++ src/components/FindUserPage/FindUsersPageClassComponent.js
@@ -3,14 +3,14 @@ import React from "react";
 import Users from "./Users/Users";
 
 class FindUsersPage extends React.Component {
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
         this.props.setUsers(response.data.items);
       });
   }
+
   onFollow = (id) => {
     this.props.follow(id);
   };
@@ -20,21 +20,18 @@ class FindUsersPage extends React.Component {
   };
 
   render() {
-    console.log("test", this.users);
     return (
       <div>
-        <div>
-          {this.props.users.map((user) => (
-            <Users
-              photo={user.photos.small}
-              followed={user.followed}
-              name={user.name}
-              status={user.status}
-              onFollow={() => this.onFollow(user.id)}
-              onUnFollow={() => this.onUnFollow(user.id)}
-            />
-          ))}
-        </div>
+        {this.props.users.map((user) => (
+          <Users
+            photo={user.photos.small}
+            followed={user.followed}
+            name={user.name}
+            status={user.status}
+            onFollow={() => this.onFollow(user.id)}
+            onUnFollow={() => this.onUnFollow(user.id)}
+          />
+        ))}
       </div>
     );
   }

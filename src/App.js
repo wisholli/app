e2117ce1a0450c78d013fecd7214710bea483a56
wisholli <osribna4@gmail.com>
2022-12-27 17:34:51 +0200

    React.lazy React.Suspense

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Nav from "./components/Nav/Nav";
 import MusicPage from "./components/Music/Musicpage";
 import NewsPage from "./components/NewsPage/NewsPage";
 import MesageContainer from "./components/Message/MesageContainer";
-import UsersContainer from "./components/FindUserPage/FindUsersContainer";
-import HomepageContainer from "./components/Homepage/HomepageContainer";
+// import UsersContainer from "./components/FindUserPage/FindUsersContainer";
+// import HomepageContainer from "./components/Homepage/HomepageContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
-import React from "react";
+import React, { Suspense } from "react";
 import { initializeApp } from "./redux/appReducer";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import Preloader from "./components/common/Preloader/Preloader";
+
+const HomepageContainer = React.lazy(() =>
+  import("./components/Homepage/HomepageContainer")
+);
+const UsersContainer = React.lazy(() =>
+  import("./components/FindUserPage/FindUsersContainer")
+);
 
 class App extends React.Component {
   componentDidMount() {
@@ -27,16 +34,26 @@ class App extends React.Component {
         <div className="app-wrapper">
           <HeaderContainer />
           <Nav />
-          <Routes>
-            <Route path="/" element={<HomepageContainer />}>
-              <Route path=":userId" element={<HomepageContainer />}></Route>
-            </Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route exact path="/message" element={<MesageContainer />}></Route>
-            <Route exact path="/music" element={<MusicPage />}></Route>
-            <Route exact path="/news" element={<NewsPage />}></Route>
-            <Route exact path="/findusers" element={<UsersContainer />}></Route>
-          </Routes>
+          <Suspense fallback={<Preloader />}>
+            <Routes>
+              <Route path="/" element={<HomepageContainer />}>
+                <Route path=":userId" element={<HomepageContainer />}></Route>
+              </Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route
+                exact
+                path="/message"
+                element={<MesageContainer />}
+              ></Route>
+              <Route exact path="/music" element={<MusicPage />}></Route>
+              <Route exact path="/news" element={<NewsPage />}></Route>
+              <Route
+                exact
+                path="/findusers"
+                element={<UsersContainer />}
+              ></Route>
+            </Routes>
+          </Suspense>
         </div>
       </BrowserRouter>
     );
